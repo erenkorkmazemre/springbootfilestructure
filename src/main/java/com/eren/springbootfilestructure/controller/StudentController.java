28b313bfb5e75afa03d9f46603ae91c10a2a7307
eren <erenkorkmazemre@gmail.com>
2022-10-10 00:37:21 +0300

    feat: student log4j2 added.

--- src/main/java/com/eren/springbootfilestructure/controller/StudentController.java
+++ src/main/java/com/eren/springbootfilestructure/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.eren.springbootfilestructure.dto.StudentDto;
 import com.eren.springbootfilestructure.mapper.StudentMapper;
 import com.eren.springbootfilestructure.model.Student;
 import com.eren.springbootfilestructure.service.StudentService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Log4j2
 @RestController
 @RequestMapping("api/v1/students")
 public class StudentController {
@@ -23,12 +25,14 @@ public class StudentController {
 
     @GetMapping("/")
     public ResponseEntity<List<Student>> getAllStudents() {
+        log.info("find all students");
         return ResponseEntity.ok(studentService.getAllStudents());
     }
 
     @PostMapping("/")
     @ResponseBody
     public ResponseEntity<Student> createStudents(@RequestBody StudentDto studentDto) {
+        log.info("create student : {}", studentDto);
         Student student = studentService.saveStudents(studentMapper.toStudent(studentDto));
         return new ResponseEntity<>(student, HttpStatus.ACCEPTED);
     }
