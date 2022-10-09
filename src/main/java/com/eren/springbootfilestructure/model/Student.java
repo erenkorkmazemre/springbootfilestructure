dd240dfeb1f858bc33d642292ddc00498d6fd340
eren <erenkorkmazemre@gmail.com>
2022-10-09 22:48:26 +0300

    feat: student model fields added

--- src/main/java/com/eren/springbootfilestructure/model/Student.java
+++ src/main/java/com/eren/springbootfilestructure/model/Student.java
@@ -1,18 +1,32 @@
 package com.eren.springbootfilestructure.model;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "namee")
+    @Column(name = "name")
     private String name;
+    @Column(name = "surname")
     private String surname;
+    @Builder.Default
+    @Column(name = "is_active")
+    private Boolean isActive = Boolean.TRUE;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "created_time")
+    private LocalDateTime createdTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "last_updated")
+    private LocalDateTime lastUpdated;
+
 
 }
